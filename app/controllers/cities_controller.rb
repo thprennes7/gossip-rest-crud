0b9dc997c1951d1be28dc16608e174dc195c6897
guhurak <florianeven.pro@laposte.net>
2019-02-07 22:32:24 +0100

    yeah plus que du css

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,6 +5,7 @@ class CitiesController < ApplicationController
 
   def show
     @selected_city = City.find_by(id: params[:id])
+    @all_likes = Like.all
     @selected_potins = Potin.where(city_id: @selected_city.id)
   end
 end
