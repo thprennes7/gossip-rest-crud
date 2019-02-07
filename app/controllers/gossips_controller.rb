e2a422c327a9a7158af357349c7588f79b6c73d0
guhurak <florianeven.pro@laposte.net>
2019-02-07 12:52:46 +0100

    edition suppression

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -9,4 +9,17 @@ class GossipsController < ApplicationController
 		p = Potin.create(title: params[:title], content: params[:content], user_id: User.all.sample.id)
 		redirect_to gossips_path
 	end
+	def edit
+		@selected_gossip = Potin.find(params[:id])
+	end
+	def update
+		@selected_gossip = Potin.find(params[:id])
+		@selected_gossip.update(title: params[:title], content: params[:content])
+		redirect_to gossips_path
+	end
+	def destroy
+		@selected_gossip = Potin.find(params[:id])
+		@selected_gossip.destroy
+		redirect_to gossips_path
+	end
 end
